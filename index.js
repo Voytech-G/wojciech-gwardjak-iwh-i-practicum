9e459d7cce4159ace4ab5405eaa56d908bf93a9a
Wojciech Gwardjak <wojciech.gwardjak@ziflow.com>
2023-12-07 14:26:06 +0100

    homepage and update views

--- index.js
+++ index.js
@@ -8,15 +8,46 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-eu1-555d23f8-3ebb-4879-9a48-69bcf8d03732';
+const OBJECT_TYPE_ID = "2-120537120";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const cars = `https://api.hubspot.com/crm/v3/schemas/${OBJECT_TYPE_ID}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(cars, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Homepage | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    const cars = `https://api.hubspot.com/crm/v3/schemas/${OBJECT_TYPE_ID}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        res.render('cars', { title: 'Contacts | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
